68f08bd9a424141d6b23623d115f4de124d9e613
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-02-04 20:05:13 +0300

    4 Şubat 2021

--- GameSaleCompany/Program.cs
+++ GameSaleCompany/Program.cs
@@ -4,6 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Bir oyun yazmak istiyorsunuz.
+//Bu yazılım için backend kodlarını C# ile geliştirmeyi planlıyoruz.
+//Yeni üye, satış ve kampanya yönetimi yapılması isteniyor.
+//Nesnelere ait özellikleri istediğiniz gibi verebilirsiniz.
+//Burada amaç yazdığınız kodun kalitesidir.
+//Ödevde gereksinimleri tam anlamadığınız durum benim için önemli değil,
+//kendinize göre mantık geliştirebilirsiniz. Dediğim gibi kod kalitesiyle ilgileniyoruz şu an :)
+
+//Gereksinimler
+//1.Oyuncuların sisteme kayıt olabileceği,
+//bilgilerini güncelleyebileceği,
+//kayıtlarını silebileceği bir ortamı simüle ediniz.
+//Müşteri bilgilerinin doğruluğunu e-devlet sistemlerini kullanarak doğrulama yapmak istiyoruz.
+//(E-devlet sistemlerinde doğrulama TcNo, Ad, Soyad, DoğumYılı bilgileriyle yapılır.
+//Bunu yapacak servisi simüle etmeniz yeterlidir.)
+//2.Oyun satışı yapılabilecek satış ortamını simüle ediniz.
+//( Yapılan satışlar oyuncu ile ilişkilendirilmelidir. Oyuncunun parametre olarak metotta olmasını kastediyorum.)
+//3.Sisteme yeni kampanya girişi, kampanyanın silinmesi ve güncellenmesi imkanlarını simule ediniz.
+//4.Satışlarda kampanya entegrasyonunu simüle ediniz.
+//5.Ödevinizi Github’a yükleyiniz. Github linkinizi paylaşınız.
+//6. Diğer arkadaşlarınızın Github kodlarını inceleyiniz. Ona yıldız vermeyi unutmayınız :)
+
+
+
 namespace GameSaleCompany
 {
     class Program
